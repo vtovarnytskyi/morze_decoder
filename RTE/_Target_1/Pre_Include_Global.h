ef9f9ccacda0c80375daa40da132d73e78f142e0
vtovarnytskyi <vasyl.ter2004@gmail.com>
2024-02-07 17:17:47 +0200

    add project

---
+++ RTE/_Target_1/Pre_Include_Global.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'cryptograph' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* Keil::Device&STM32CubeMX:STM32Cube HAL@1.11.3 */
+#define USE_HAL_DRIVER
+          #define USE_FULL_LL_DRIVER
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,34 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'cryptograph' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+/* Keil::CMSIS Driver:USART:USART Async@1.1.0 */
+#define RTE_Drivers_USART1_Async        /* Driver USART1 */
+        #define RTE_Drivers_USART2_Async        /* Driver USART2 */
+        #define RTE_Drivers_USART3_Async        /* Driver USART3 */
+        #define RTE_Drivers_USART4_Async        /* Driver USART4 */
+        #define RTE_Drivers_USART5_Async        /* Driver USART5 */
+        #define RTE_Drivers_USART6_Async        /* Driver USART6 */
+        #define RTE_Drivers_USART7_Async        /* Driver USART7 */
+        #define RTE_Drivers_USART8_Async        /* Driver USART8 */
+/* Keil::Device&STM32CubeMX:STM32Cube Framework:STM32CubeMX@1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device&STM32CubeMX:STM32Cube HAL@1.11.3 */
+#define RTE_DEVICE_CUBE_MX_HAL
+
+
+#endif /* RTE_COMPONENTS_H */
